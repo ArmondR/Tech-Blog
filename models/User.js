f90f276608f8f4598de1ae6ffc48df18fcabaab6
ARod <ajr.rodriguez095@gmail.com>
2022-04-08 19:39:34 -0400

    created login validation

--- models/User.js
+++ models/User.js
@@ -4,7 +4,11 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create user model
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define user table columns and configuration
 User.init(
