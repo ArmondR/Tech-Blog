33ca8982e6fd70efa550cc44b1cb64c6652d666e
ARod <ajr.rodriguez095@gmail.com>
2022-04-10 18:23:18 -0400

    login and sign up forms collects user input and fetch correct api request

---
+++ public/javascript/login.js
@@ -0,0 +1,20 @@
+function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        }).then((response) => {console.log(response)})
+    }
+
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
