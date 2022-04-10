6e66c23b51400ee8094cc6ea9abfe61aa9a56442
ARod <ajr.rodriguez095@gmail.com>
2022-04-10 17:36:17 -0400

    homepage render setup

---
+++ controllers/home-routes.js
@@ -0,0 +1,39 @@
+// home page and login page routes
+
+const router = require('express').Router();
+const res = require('express/lib/response');
+const sequelize = require('../config/connection');
+const { Post, User, Comment} = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'content', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true }));
+    // pass a post object into the homepage template
+    res.render('homepage', { posts });
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
-// error handeling for request that dont exist.
+// error handling for request that dont exist.
 router.use((req, res) => {
     res.status(404).end();
 });
