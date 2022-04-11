f3ffc7afab40147c0a00a83c0234aad401ac1587
ARod <ajr.rodriguez095@gmail.com>
2022-04-10 23:20:57 -0400

    added date formatting

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const routes = require('./controllers');
 const sequelize =  require('./config/connection');
 const path = require('path');
 
+const helpers = require('./utils/helpers');
+
 require('dotenv').config();
 
 // implement express session (cookies)
@@ -22,7 +24,7 @@ const sess = {
 
 // configure handlebars.js as template engine of choice
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
